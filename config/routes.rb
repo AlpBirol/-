549690aa73b611c3645d2ad50e3f25305161d8a6
AlpBirol <40274530+AlpBirol@users.noreply.github.com>
2018-07-08 20:42:23 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  as :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :drops
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'drops#index'
